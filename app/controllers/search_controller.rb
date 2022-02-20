5cfe87f6fba87aca143f532eef8f03cd59a09845
dankeezer <dankeezer@gmail.com>
2022-02-20 16:00:37 -0500

    Update search request

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,8 @@
 class SearchController < ApplicationController
   def index
-    return render unless params[:search].present?
+    return render unless params[:query].present?
 
-    search = PgSearch.multisearch(params[:search])
+    search = PgSearch.multisearch(params[:query])
     @search_results = search.map { |s| s.searchable_type.constantize.find(s.searchable_id) }
 
     render
