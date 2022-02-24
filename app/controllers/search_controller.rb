5c4acf4ff1221a34cf48f46c5715a317a33d3ce2
dankeezer <dankeezer@gmail.com>
2022-02-24 16:56:45 -0500

    Update search with simplified controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,31 +1,31 @@
 class SearchController < ApplicationController
   def index
     @machines, @tournaments = [[],[]]
-    return render unless params[:query].present?
+    return render unless params["query"].present?
 
     @machines, @tournaments = search(params["query"])
 
-    render
-  end
-
-  def new
-    @machines, @tournaments = search(params["query"])
-
-    render json: {
-      machines: @machines,
-      tournaments: @tournaments
-    }.to_json
+    if request.content_type == "application/json"
+      render json: {
+        machines: @machines,
+        tournaments: @tournaments
+      }.to_json
+    else
+      render # html
+    end
   end
 
   private
 
   def search(query)
+    # Four DB queries is a lot for inline JS, we can cache it if there's a performance hit.
+
     machines = Machine.where(["name ILIKE ?", "%#{query}%"])
     tournaments = Tournament.where(["name ILIKE ?", "%#{query}%"])
 
-    machines += Machine.search(query) if machines.empty?
-    tournaments += Tournament.search(query) if tournaments.empty?
+    machines += Machine.search(query)
+    tournaments += Tournament.search(query)
 
-    [machines.uniq.sort_by(&:name), tournaments.uniq.sort_by(&:name)]
+    [machines.uniq.sort_by(&:name), tournaments.uniq.sort_by(&:event_started_at).reverse]
   end
 end
