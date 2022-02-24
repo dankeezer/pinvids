4513ac7ccaad0024913ab85ce60e0040ff76c7a3
dankeezer <dankeezer@gmail.com>
2022-02-24 17:09:26 -0500

    Add down migration to pg extensions

--- db/migrate/20220223232333_add_pg_extensions_to_db.rb
+++ db/migrate/20220223232333_add_pg_extensions_to_db.rb
@@ -1,7 +1,13 @@
 class AddPgExtensionsToDb < ActiveRecord::Migration[7.0]
-  def change
+  def up
     execute "create extension unaccent"
     execute "create extension pg_trgm;"
     execute "create extension fuzzystrmatch;"
+  end
+
+  def down
+    execute "drop extension unaccent"
+    execute "drop extension pg_trgm;"
+    execute "drop extension fuzzystrmatch;"
   end
 end
